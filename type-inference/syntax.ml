1c5b3ec3ef2f26d4190a124c7194baaf53a0d288
Takuya Kuwahara <taakuu19@gmail.com>
2015-01-23 19:32:54 +0900

    Improved pp_ty display

--- type-inference/syntax.ml
+++ type-inference/syntax.ml
@@ -29,14 +29,23 @@ type ty =
   | TyVar of tyvar
   | TyFun of ty * ty
 
-let rec pp_ty = function
+let rec pp_ty ty =
+  let memo = ref [] in
+  let get_num v =
+    let rec body l cnt =
+      match l with
+        [] -> memo := !memo @ [v]; cnt
+      | hd :: tl -> if v = hd then cnt else body tl (cnt + 1)
+  in body !memo 0 in
+  let rec inner_print_ty = function
     TyInt -> print_string "int"
   | TyBool -> print_string "bool"
-  | TyVar (var) -> print_string (Printf.sprintf "'%c" (char_of_int ((int_of_char 'a') + var)))
+  | TyVar (var) -> print_string ("`" ^ (Char.escaped (char_of_int ((int_of_char 'a') + (get_num var)))))
   | TyFun (ty1, ty2) -> 
       (match ty1 with
-        TyFun (_, _) -> print_string "("; pp_ty ty1; print_string ") -> "; pp_ty ty2;
-      | _ -> pp_ty ty1; print_string " -> "; pp_ty ty2;)
+        TyFun (_, _) -> print_string "("; inner_print_ty ty1; print_string ") -> "; inner_print_ty ty2;
+      | _ -> inner_print_ty ty1; print_string " -> "; inner_print_ty ty2;)
+  in inner_print_ty ty
 
 let fresh_tyvar =
   let counter = ref 0 in
